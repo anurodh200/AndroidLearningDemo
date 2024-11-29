f96dd6b9aa3abbc276be743f2629ea68c80b6e8e
anurodh200 <anrodhiit123@gmail.com>
2024-11-29 12:17:46 +0530

    Web View Added

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,39 +2,59 @@ package com.example.demo
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val webBtn = findViewById<CardView>(R.id.cardWeb)
-        val cameraBtn = findViewById<CardView>(R.id.cardCamera)
 
-        webBtn.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse("https://desibakery.co.in/")
-            startActivity(intent)
-        }
+        val webView = findViewById<WebView>(R.id.webId)
+        webViewSetup(webView)
 
-        cameraBtn.setOnClickListener {
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivity(intent)
-        }
+
+
+//        webBtn.setOnClickListener {
+//            val intent = Intent(Intent.ACTION_VIEW)
+//            intent.data = Uri.parse("https://desibakery.co.in/")
+//            startActivity(intent)
+//        }
+
+//        cameraBtn.setOnClickListener {
+//            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//            startActivity(intent)
+//        }
 
 //        btnIntent.setOnClickListener {
 //            //Open a new Activity
 //            intent = Intent(applicationContext, IntentScreen::class.java)
 //            startActivity(intent)
 //        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webViewSetup(webView: WebView?) {
+        webView?.webViewClient = WebViewClient()
+
+        webView?.apply {
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+            loadUrl("https://desibakery.co.in/")
+        }
+
     }
 }
